cf4de2d947d09a6ceff340154ed9298f7cfad8f9
shalaka-shrawage <shalakashrawage246@gmail.com>
2022-01-10 18:16:13 +0530

    mycommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Header';
+import Left1 from './Left1';
+
+function App() {
+ return(
+    <div className='main'>
+     <Header/>
+     <Left1/>
+     
+    </div>
+ );
+}
+
+export default App;
